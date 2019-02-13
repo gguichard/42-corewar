6a5163aa4ed58b1353d0c68485caa93f3d08fa2e
Robert VALENTIN <rvalenti@e1r6p3.42.fr>
2019-02-13 22:40:42 +0100

    creation du dir champs et ajout du champions unicorn qui boucle sur sa partie memoire a regarder

---
+++ champs/unicorn.s
@@ -0,0 +1,13 @@
+.name "unicorn"
+.comment "blblblblblblblblblblblblblblbl"
+
+init: sti r1,%:live,%1
+ld %90,r2
+ld %6,r3
+ld %0,r4
+
+live:live%1
+sti r4,r2,r4
+add r4,r3,r4
+and r1,%0,r1
+zjmp%:live
