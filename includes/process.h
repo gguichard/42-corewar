32c394c97ffa807b1b907d9fcc66872e61fe42c7
Gwennaël Guichard <gwennaelguich@gmail.com>
2019-02-13 08:01:20 +0100

    Debut VM, gestion de l'option -dump

--- includes/process.h
+++ includes/process.h
@@ -1,32 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   corewar.h                                          :+:      :+:    :+:   */
+/*   process.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gguichar <gguichar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/02/13 04:35:43 by gguichar          #+#    #+#             */
-/*   Updated: 2019/02/13 07:31:47 by gguichar         ###   ########.fr       */
+/*   Created: 2019/02/13 06:03:00 by gguichar          #+#    #+#             */
+/*   Updated: 2019/02/13 07:15:35 by gguichar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef COREWAR_H
-# define COREWAR_H
+#ifndef PROCESS_H
+# define PROCESS_H
 
-# include "libft.h"
+# include <stdint.h>
 # include "op.h"
-# include "process.h"
 
-typedef struct s_env	t_env;
+typedef struct s_process	t_process;
 
-struct	s_env
+struct s_process
 {
-	unsigned char	arena[MEM_SIZE];
-	t_list			*process_lst;
-	int				cur_cycle;
-	int				dump_cycles;
-	int				cycle_to_die;
-	int				cycle_before_die;
+	unsigned char	reg[REG_NUMBER][REG_SIZE];
+	uint32_t		pc_reg;
+	int				carry:1;
 };
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,15 +6,51 @@
 /*   By: gguichar <gguichar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/13 04:35:03 by gguichar          #+#    #+#             */
-/*   Updated: 2019/02/13 04:35:36 by gguichar         ###   ########.fr       */
+/*   Updated: 2019/02/13 07:34:08 by gguichar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <limits.h>
+#include "libft.h"
+#include "op.h"
 #include "corewar.h"
 
-int	main(int argc, char **argv)
+static int	parse_opts(t_env *env, char **argv)
 {
+	int		cur_arg;
+	long	dump_cycles;
+	char	*endptr;
+
+	cur_arg = 1;
+	if (ft_strequ("-dump", argv[cur_arg]))
+	{
+		if (argv[cur_arg + 1] == NULL
+				|| !(dump_cycles = ft_strtol(argv[cur_arg + 1], &endptr, 10))
+				|| dump_cycles < 0 || dump_cycles > INT_MAX
+				|| *endptr != '\0')
+		{
+			ft_dprintf(2, "corewar: error: dump cycles should be an integer "
+					"between 0 and %d\n", INT_MAX);
+			return (0);
+		}
+		cur_arg += 2;
+		env->dump_cycles = (int)dump_cycles;
+	}
+	return (1);
+}
+
+int			main(int argc, char **argv)
+{
+	t_env	env;
+
 	(void)argc;
-	(void)argv;
-	return (0);
+	ft_memset(&env, 0, sizeof(t_env));
+	env.cycle_to_die = CYCLE_TO_DIE;
+	env.cycle_before_die = env.cycle_to_die;
+	env.dump_cycles = -1;
+	if (parse_opts(&env, argv))
+	{
+		return (0);
+	}
+	return (1);
 }
