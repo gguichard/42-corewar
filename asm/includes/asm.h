6723eb4aa5f286a83f381dc7e4be0d1f78f75b52
William TA <wta@e3r2p19.42.fr>
2019-02-14 07:21:25 +0100

    WIP 0.2

--- asm/includes/asm.h
+++ asm/includes/asm.h
@@ -6,7 +6,7 @@
 /*   By: wta <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/13 05:25:14 by wta               #+#    #+#             */
-/*   Updated: 2019/02/14 04:08:16 by rvalenti         ###   ########.fr       */
+/*   Updated: 2019/02/14 06:51:53 by wta              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,29 +32,41 @@ typedef struct	s_list_ref
 	t_list		*tail;
 }				t_list_ref;
 
+typedef struct	s_label
+{
+	char	*name;
+	int		offset;
+}				t_label;
+
 typedef struct	s_type
 {
-	int		arg1;
-	int		arg2;
-	int		arg3;
+	char	arg1;
+	char	arg2;
+	char	arg3;
 }				t_type;
 
 typedef struct	s_op
 {
-	char		*op;
-	int			argc;
-	t_type		arg_type;
-	char		opcode;
-	int			cycle;
-	char		*name;
-	char		encoding	: 1;
-	char		direct		: 1;
+	char			*op;
+	int				argc;
+	t_type			arg_type;
+	char			opcode;
+	int				cycle;
+	char			*name;
+	char			encoding	: 1;
+	char			direct		: 1;
 }				t_op;
+
+typedef struct	s_param
+{
+	t_op		op;
+	int			argv[3];
+}				t_param;
 
 typedef struct	s_data
 {
 	t_header	header;
-	t_op		op_tab[17];
+	t_op		op_tab[16];
 	t_list_ref	list;
 }				t_data;
 
--- asm/src/read_file.c
+++ asm/src/read_file.c
@@ -6,7 +6,7 @@
 /*   By: wta <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/13 05:24:10 by wta               #+#    #+#             */
-/*   Updated: 2019/02/14 03:36:18 by wta              ###   ########.fr       */
+/*   Updated: 2019/02/14 06:53:25 by wta              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,18 +31,37 @@ t_error	check_first_line(int fd)
 	return (ERR_NOERROR);
 }
 
-t_error	is_skippable(int fd, char **line)
+char	*skip_tab_n_space(char *str)
 {
-	int	ret;
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] == ' ' || str[i] == '\t')
+			i += 1;
+		else
+			break ;
+	}
+	if (str[i] == '\0')
+		return (NULL);
+	return (&str[i]);
+}
+
+t_error	skip_useless(int fd, char **line)
+{
+	char	*str;
+	int		ret;
 
 	while ((ret = get_next_line(fd, line)) > 0)
 	{
-		if ((*line)[0] != '\n' && (*line)[0] != '\t' && (*line)[0] != ' ')
+		str = skip_tab_n_space(*line);
+		if (str[0] != '#' || str[0] != '\0')
 			break ;
 		free(*line);
 	}
-	if (ret == -1)
-		return (ERR_GNL);
+	if (ret <= 0)
+		return (ERR_BADFMT);
 	return (ERR_NOERROR);
 }
 
@@ -102,9 +121,10 @@ t_error	get_name(t_data *data, int fd, char **line)
 	char	*str;
 
 	err_id = ERR_NOERROR;
-	str = NULL;
-	if ((ft_strnequ(*line, ".name", 5) == 0)
-			|| ((str = get_dquote((*line) + 5)) == NULL))
+	if ((str = skip_tab_n_space(*line)) == NULL)
+		return (ERR_BADFMT);
+	if ((ft_strnequ(str, NAME_CMD_STRING, 5) == 0)
+			|| ((str = get_dquote((str) + 5)) == NULL))
 		err_id = ERR_BADFMT;
 	while (err_id == ERR_NOERROR && (needle = ft_strchr(str, '"')) == NULL)
 		err_id = fill_prog_name(data, fd, &str, line);
@@ -142,10 +162,9 @@ t_error	get_comment(t_data *data, int fd, char **line)
 	char	*str;
 
 	err_id = ERR_NOERROR;
-	str = NULL;
-	if (get_next_line(fd, line) <= 0)
+	if ((str = skip_tab_n_space(*line)) == NULL)
 		return (ERR_BADFMT);
-	if ((ft_strnequ(*line, ".comment", 8) == 0)
+	if ((ft_strnequ(*line, COMMENT_CMD_STRING, 8) == 0)
 			|| ((str = get_dquote((*line) + 8)) == NULL))
 		err_id = ERR_BADFMT;
 	while (err_id == ERR_NOERROR && (needle = ft_strchr(str, '"')) == NULL)
@@ -166,29 +185,80 @@ void	set_op_tab(t_op op_tab[])
 {
 	op_tab[0] = (t_op){"live", 1, {T_DIR, 0, 0}, 1, 10, "alive", 0, 0};
 	op_tab[1] = (t_op){"ld", 2, {T_DIR | T_IND, T_REG, 0}, 2, 5, "load", 1, 0};
-	op_tab[3] = (t_op){"st", 2, {T_REG, T_IND | T_REG, 0}, 3, 5, "store", 1, 0};
-	op_tab[4] = (t_op){"add", 3, {T_REG, T_REG, T_REG}, 4, 10, "addition", 1
+	op_tab[2] = (t_op){"st", 2, {T_REG, T_IND | T_REG, 0}, 3, 5, "store", 1, 0};
+	op_tab[3] = (t_op){"add", 3, {T_REG, T_REG, T_REG}, 4, 10, "addition", 1
 		, 0};
-	op_tab[5] = (t_op){"sub", 3, {T_REG, T_REG, T_REG}, 5, 10, "soustraction"
+	op_tab[4] = (t_op){"sub", 3, {T_REG, T_REG, T_REG}, 5, 10, "soustraction"
 		, 1, 0};
-	op_tab[6] = (t_op){"and", 3, {T_REG | T_DIR | T_IND, T_REG | T_IND | T_DIR
+	op_tab[5] = (t_op){"and", 3, {T_REG | T_DIR | T_IND, T_REG | T_IND | T_DIR
 		, T_REG}, 6, 6, "et (and  r1, r2, r3   r1&r2 -> r3", 1, 0};
-	op_tab[7] = (t_op){"or", 3, {T_REG | T_IND | T_DIR, T_REG | T_IND | T_DIR
+	op_tab[6] = (t_op){"or", 3, {T_REG | T_IND | T_DIR, T_REG | T_IND | T_DIR
 		, T_REG}, 7, 6, "ou  (or   r1, r2, r3   r1 | r2 -> r3", 1, 0};
-	op_tab[8] = (t_op){"xor", 3, {T_REG | T_IND | T_DIR, T_REG | T_IND | T_DIR
+	op_tab[7] = (t_op){"xor", 3, {T_REG | T_IND | T_DIR, T_REG | T_IND | T_DIR
 		, T_REG}, 8, 6, "ou (xor  r1, r2, r3   r1^r2 -> r3", 1, 0};
-	op_tab[9] = (t_op){"zjmp", 1, {T_DIR, 0, 0}, 9, 20, "jump if zero", 0, 1};
-	op_tab[10] = (t_op){"ldi", 3, {T_REG | T_DIR | T_IND, T_DIR | T_REG, T_REG}
+	op_tab[8] = (t_op){"zjmp", 1, {T_DIR, 0, 0}, 9, 20, "jump if zero", 0, 1};
+	op_tab[9] = (t_op){"ldi", 3, {T_REG | T_DIR | T_IND, T_DIR | T_REG, T_REG}
 		, 10, 25, "load index", 1, 1};
-	op_tab[11] = (t_op){"sti", 3, {T_REG, T_REG | T_DIR | T_IND, T_DIR | T_REG}
+	op_tab[10] = (t_op){"sti", 3, {T_REG, T_REG | T_DIR | T_IND, T_DIR | T_REG}
 		, 11, 25, "store index", 1, 1};
-	op_tab[12] = (t_op){"fork", 1, {T_DIR, 0, 0}, 12, 800, "fork", 0, 1};
-	op_tab[13] = (t_op){"lld", 2, {T_DIR | T_IND, T_REG, 0}, 13, 10
+	op_tab[11] = (t_op){"fork", 1, {T_DIR, 0, 0}, 12, 800, "fork", 0, 1};
+	op_tab[12] = (t_op){"lld", 2, {T_DIR | T_IND, T_REG, 0}, 13, 10
 		, "long load", 1, 0};
-	op_tab[14] = (t_op){"lldi", 3, {T_REG | T_DIR | T_IND, T_DIR | T_REG, T_REG}
+	op_tab[13] = (t_op){"lldi", 3, {T_REG | T_DIR | T_IND, T_DIR | T_REG, T_REG}
 		, 14, 50, "long load index", 1, 1};
-	op_tab[15] = (t_op){"lfork", 1, {T_DIR, 0, 0}, 15, 1000, "long fork", 0, 1};
-	op_tab[16] = (t_op){"aff", 1, {T_REG, 0, 0}, 16, 2, "aff", 1, 0};
+	op_tab[14] = (t_op){"lfork", 1, {T_DIR, 0, 0}, 15, 1000, "long fork", 0, 1};
+	op_tab[15] = (t_op){"aff", 1, {T_REG, 0, 0}, 16, 2, "aff", 1, 0};
+}
+
+int		is_op(t_op op_tab[], char *str)
+{
+	t_error	err_id;
+	int		i;
+
+	err_id = ERR_NOERROR;
+	i = 0;
+	while (i < 16)
+	{
+		if (ft_strnequ(op_tab[i].op, str, ft_strlen(op_tab[i].op)) == 1)
+			return (1);
+		i += 1;
+	}
+	return (0);
+}
+
+int		is_label(char *str)
+{
+	(void)str;
+	return (1);
+}
+
+t_error	manage_op(char *str)
+{
+	(void)str;
+	return (ERR_NOERROR);
+}
+
+t_error	manage_label(char *str)
+{
+	(void)str;
+	return (ERR_NOERROR);
+}
+
+t_error	manage_inst(t_data *data, int fd, char **line)
+{
+	t_error	err_id;
+	char	*str;
+	(void)fd;
+	err_id = ERR_NOERROR;
+	if ((str = skip_tab_n_space(*line)) == NULL)
+		return (ERR_BADFMT);
+	if (is_op(data->op_tab, str) == 1)
+		err_id = manage_op(str);
+	else if (is_label(str) == 1)
+		err_id = manage_label(str);
+	else
+		err_id = ERR_BADFMT;
+	return (err_id);
 }
 
 t_error	read_file(char *file, t_data *data)
@@ -202,14 +272,17 @@ t_error	read_file(char *file, t_data *data)
 	err_id = check_first_line(fd);
 	if (err_id == ERR_NOERROR)
 	{
-		if ((err_id = is_skippable(fd, &line)) != ERR_NOERROR)
+		if ((err_id = skip_useless(fd, &line)) != ERR_NOERROR)
 			return (err_id);
 		if ((err_id = get_name(data, fd, &line)) != ERR_NOERROR)
 			return (err_id);
+		if ((err_id = skip_useless(fd, &line)) != ERR_NOERROR)
+			return (err_id);
 		if ((err_id = get_comment(data, fd, &line)) != ERR_NOERROR)
 			return (err_id);
-		if ((err_id = is_skippable(fd, &line)) != ERR_NOERROR)
+		if ((err_id = skip_useless(fd, &line)) != ERR_NOERROR)
 			return (err_id);
+		err_id = manage_inst(data, fd, &line);
 	}
 	close(fd);
 	return (err_id);
