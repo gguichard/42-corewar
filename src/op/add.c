349c72a9845847443ef396c486fa97b35b9e11fd
vifonne <fonne.victor@gmail.com>
2019-02-15 22:55:12 +0100

    fonction add ok

--- src/op/add.c
+++ src/op/add.c
@@ -6,7 +6,7 @@
 /*   By: vifonne <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/15 21:55:32 by vifonne           #+#    #+#             */
-/*   Updated: 2019/02/15 22:49:39 by vifonne          ###   ########.fr       */
+/*   Updated: 2019/02/15 22:55:02 by vifonne          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,20 +28,20 @@ int			add(t_env *env, t_process *cur_process, unsigned char *str)
 {
 	t_decode		decode;
 	int				args[3];
+	int				tmp;
 	int				i;
-	unsigned char	tmp[REG_SIZE];
 
-	i = -1;
-	while (++i < 3)
+	(void)env;
+	i = 0;
+	while (i < 3)
+	{
 		args[i] = *((int *)decode.tab[i].value);
+		i++;
+	}
 	if (!get_args(str + 1, REG_CODE << 6 | REG_CODE << 4 | REG_CODE << 2, &decode)
 			|| !reg_isvalid(args))
 		return (0);
-	i = -1;
-	while (++i < REG_SIZE)
-		tmp[i] = cur_process->reg[args[1]][i] + cur_process->reg[args[2]][i];
-	//write(1, tmp, REG_SIZE);
-	ft_memcpy(cur_process->reg[args[3]], tmp, REG_SIZE);
-	(void)env;
+	tmp = *((int *)cur_process->reg[args[0] - 1]) + *((int *)cur_process->reg[args[1] - 1]);
+	ft_memcpy(cur_process->reg[args[2] - 1], &tmp, sizeof(int));
 	return (3);
 }
