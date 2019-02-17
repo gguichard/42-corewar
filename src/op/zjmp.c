31dd468c75c64f3fdb0f9128527fa53e44a833f3
Gwennaël Guichard <gwennaelguich@gmail.com>
2019-02-17 04:47:21 +0100

    Ajout zjmp

---
+++ src/op/zjmp.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   zjmp.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gguichar <gguichar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/02/17 02:17:41 by gguichar          #+#    #+#             */
+/*   Updated: 2019/02/17 04:46:08 by gguichar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "corewar.h"
+#include "func_op.h"
+#include "op.h"
+
+int	zjmp(t_env *env, t_process *cur_process, unsigned char *bytes)
+{
+	int			ret;
+	t_decode	decode;
+
+	(void)env;
+	ret = get_args(bytes + 1, DIR_CODE << 6, &decode, 1);
+	if (decode.tab[0].type == DIR_CODE && cur_process->carry == 1)
+		return (*((short *)decode.tab[0].value) % IDX_MOD - 1);
+	return (ret);
+}
