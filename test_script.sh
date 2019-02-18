e362d35f260f6e10716b6a2979edee5e97f97d07
vifonne <fonne.victor@gmail.com>
2019-02-18 02:26:43 +0100

    script

--- test_script.sh
+++ test_script.sh
@@ -5,13 +5,8 @@ echo -n "Dump offset: "
 read offset
 for i in `seq $offset $max_dump`
 do
-	~/vm_champ/corewar -d $i $1 $2 > zaz
-	./corewar -dump $i -n -1 $1 -n -2 $2 > our
-	sed "1,2d" zaz > tmp_zaz
-	mv tmp_zaz zaz
-	sed "1,1d" our > tmp_our
-	sed '$d' tmp_our > our
-	rm -f tmp_our
+	~/vm_champ/corewar -d $i $1 $2 | grep '^0x' > zaz
+	./corewar -dump $i -n -1 $1 -n -2 $2 | grep '^0x' > our
 	diff zaz our > diff
 	echo "Cycle: $i"
 	cat diff
