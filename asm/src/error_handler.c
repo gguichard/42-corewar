c37299e237930fd15c260877bb6eda1db59e7818
William TA <wta@e2r9p8.42.fr>
2019-02-20 02:04:17 +0100

    Messages d'erreur

--- asm/src/error_handler.c
+++ asm/src/error_handler.c
@@ -6,21 +6,30 @@
 /*   By: wta <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/13 05:24:35 by wta               #+#    #+#             */
-/*   Updated: 2019/02/18 06:25:42 by rvalenti         ###   ########.fr       */
+/*   Updated: 2019/02/20 02:02:25 by wta              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "asm.h"
 #include <unistd.h>
+#include <errno.h>
+#include "asm.h"
 
 void	err_handler(t_error err_id)
 {
+	char	buf[255];
+
+	ft_bzero(&buf, 255);
+	ft_strcat(buf, "Terminated with error: ");
 	if (err_id == ERR_BADFMT)
-		write(2, "bad format\n", 11);
+		ft_strcat(buf, "Bad format");
 	else if (err_id == ERR_GNL)
-		write(2, "Read Error\n", 11);
+		ft_strcat(buf, "Read error");
 	else if (err_id == ERR_NAME)
-		write(2, "bad header\n", 11);
+		ft_strcat(buf, "Bad header");
 	else if (err_id == ERR_MALLOC)
-		write(2, "Malloc Error\n", 13);
+		ft_strcat(buf, "Malloc error");
+	else if (err_id == ERR_ERRNO)
+		ft_strcat(buf, strerror(errno));
+	ft_strcat(buf, "\n");
+	write(2, buf, ft_strlen(buf));
 }
