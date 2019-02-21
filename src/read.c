3917bb6ada2f4bc440c2895131b6f3ace370289f
Robert VALENTIN <rvalenti@e2r9p10.42.fr>
2019-02-21 07:25:32 +0100

    gestion des invalides prog size bytes

--- src/read.c
+++ src/read.c
@@ -6,10 +6,11 @@
 /*   By: vifonne <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/13 05:23:27 by vifonne           #+#    #+#             */
-/*   Updated: 2019/02/14 03:40:10 by vifonne          ###   ########.fr       */
+/*   Updated: 2019/02/21 07:24:46 by rvalenti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <inttypes.h>
 #include <fcntl.h>
 #include <errno.h>
 #include <unistd.h>
@@ -17,6 +18,7 @@
 #include "libft.h"
 #include "champion.h"
 #include "parsing.h"
+#include "func_op.h"
 
 static int	read_magic(int fd, t_champ *champ)
 {
@@ -50,7 +52,20 @@ static int	read_name(int fd, t_champ *champ)
 		return (0);
 	}
 	ft_memmove(champ->header.prog_name, buf, PROG_NAME_LENGTH);
-	lseek(fd, 8, SEEK_CUR);
+	lseek(fd, 4, SEEK_CUR);
+	if ((ret = read(fd, buf, 4)) != 4)
+	{
+		ft_dprintf(2, "corewar: error: Bad champ size\n");
+		return (0);
+	}
+	swap_bytes((uint8_t *)buf, 4);
+	champ->header.prog_size = *((unsigned int *)buf);
+	if (champ->header.prog_size > CHAMP_MAX_SIZE)
+	{
+		ft_dprintf(2, "corewar: error: Champ size %u is too long\n"
+				, champ->header.prog_size);
+		return (0);
+	}
 	return (1);
 }
 
@@ -74,27 +89,15 @@ static int	read_comment(int fd, t_champ *champ)
 static int	read_prog(int fd, t_champ *champ)
 {
 	int				ret;
-	unsigned int	size;
 	unsigned char	buf[CHAMP_MAX_SIZE];
 
-	ret = read(fd, buf, CHAMP_MAX_SIZE);
-	if (ret <= 0)
+	ret = read(fd, buf, champ->header.prog_size + 1);
+	if (ret != (int)champ->header.prog_size)
 	{
 		ft_dprintf(2, "corewar: error: Bad champ code reading\n");
 		return (0);
 	}
-	size = ret;
-	if (ret == CHAMP_MAX_SIZE)
-	{
-		ret = read(fd, buf, 1);
-		if (ret > 0)
-		{
-			ft_dprintf(2, "corewar: error: Champ size must be 682 bytes\n");
-			return (0);
-		}
-	}
-	ft_memcpy(champ->prog, buf, size);
-	champ->header.prog_size = size;
+	ft_memcpy(champ->prog, buf, ret);
 	return (1);
 }
 
@@ -111,9 +114,9 @@ int			read_file(char *fn, t_champ *champ)
 	}
 	ret = 1;
 	if (!read_magic(fd, champ)
-		|| !read_name(fd, champ)
-		|| !read_comment(fd, champ)
-		|| !read_prog(fd, champ))
+			|| !read_name(fd, champ)
+			|| !read_comment(fd, champ)
+			|| !read_prog(fd, champ))
 		ret = 0;
 	close(fd);
 	return (ret);
